c19b9657db0b8495ca0cb638c5cbd4db955476bd
cut010 <cut010@users.noreply.github.com>
2026-09-09 21:59:54 -0700

    u

--- lidlt.js
+++ lidlt.js
@@ -10,8 +10,6 @@
 );
 
 
-document.head.insertAdjacentElement("afterend", scriptx);
-
 const script = document.createElement("script");
 script.src = "https://pl28979147.profitableratecpmnetwork.com/39/10/96/391096f637383e63beb239e434d5d525.js";
 document.head.appendChild(script);
